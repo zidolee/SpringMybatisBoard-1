2aeb51116f6933da886c21d21faac089ab367b42
일반 <@DESKTOP-I43I40G>
2019-05-14 13:05:06 +0900

    추가 파일들

--- src/main/java/com/springbook/view/user/JoinController.java
+++ src/main/java/com/springbook/view/user/JoinController.java
@@ -1,20 +1,28 @@
 package com.springbook.view.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.user.UserService;
 import com.springbook.biz.user.UserVO;
 import com.springbook.biz.user.impl.UserDAO;
 
 @Controller
 public class JoinController {
+	
+	@Autowired
+	UserService userService;
+	
 	@RequestMapping(value="/join.do", method=RequestMethod.POST)
 	public String jogin(UserVO vo, UserDAO userDAO) {
 		System.out.println("회원가입"); 
 		int join = 0;
-		join = userDAO.insertUser(vo);
+		join = userService.insertUserVO(vo);
 		if(join == 1) {
 			return "redirect:login.do";
 		} else {
@@ -26,4 +34,29 @@ public class JoinController {
 	public String loginView(@ModelAttribute("user") UserVO vo) {
 		return "join.jsp";
 	}
+	
+	@RequestMapping("/getUser.do")
+	public String getUser(UserVO vo, Model model) {
+		System.out.println("글 상세 조회 처리");
+		
+		model.addAttribute("board", userService.getUserVO(vo));
+		return "getBoard.jsp";
+	}
+	
+	@RequestMapping("/updateUser.do")
+	public String updateUser(@ModelAttribute("board") UserVO vo) {	
+		userService.updateUser(vo);
+		return "redirect:getBoardList.do";
+	}
+	
+	@RequestMapping("/deleteUser.do")
+	public String deletUser(UserVO vo) {
+		System.out.println("글 삭제 처리");
+		
+		userService.deleteBoard(vo);
+		return "redirect:getBoardList.do";
+	}
+	
+	
+	
 }
