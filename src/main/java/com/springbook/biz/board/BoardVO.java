5e343827e492684a3850231d5f50e4827f8c4d08
일반 <@DESKTOP-I43I40G>
2019-05-22 10:37:18 +0900

    파일 정리

--- src/main/java/com/springbook/biz/board/BoardVO.java
+++ src/main/java/com/springbook/biz/board/BoardVO.java
@@ -11,7 +11,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+//import lombok.Data;
+
 //@XmlAccessorType(XmlAccessType.FIELD)			//xml 데이터처리
+//@Data
 public class BoardVO {
 //	@XmlAttribute
 	private int seq;
@@ -26,86 +29,60 @@ public class BoardVO {
 	private String searchKeyword;
 //	@XmlTransient
 	private MultipartFile uploadFile;
-
 	public int getSeq() {
 		return seq;
 	}
-
 	public void setSeq(int seq) {
 		this.seq = seq;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getWriter() {
 		return writer;
 	}
-
 	public void setWriter(String writer) {
 		this.writer = writer;
 	}
-
 	public String getContent() {
 		return content;
 	}
-
 	public void setContent(String content) {
 		this.content = content;
 	}
-
 	public Date getRegDate() {
 		return regDate;
 	}
-
 	public void setRegDate(Date regDate) {
 		this.regDate = regDate;
 	}
-
 	public int getCnt() {
 		return cnt;
 	}
-
 	public void setCnt(int cnt) {
 		this.cnt = cnt;
 	}
-	
-	@JsonIgnore									//JSON 데이터 처리 제외
 	public String getSearchCondition() {
 		return searchCondition;
 	}
-
 	public void setSearchCondition(String searchCondition) {
 		this.searchCondition = searchCondition;
 	}
-	
-	@JsonIgnore
 	public String getSearchKeyword() {
 		return searchKeyword;
 	}
-
 	public void setSearchKeyword(String searchKeyword) {
 		this.searchKeyword = searchKeyword;
 	}
-
-	@JsonIgnore
 	public MultipartFile getUploadFile() {
 		return uploadFile;
 	}
-	
 	public void setUploadFile(MultipartFile uploadFile) {
 		this.uploadFile = uploadFile;
 	}
-	@Override
-	public String toString() {
-		return "BoardVO [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regDate="
-				+ regDate + ", cnt=" + cnt + ", searchCondition=" + searchCondition + ", searchKeyword=" + searchKeyword
-				+ "]";
-	}
+
 
 }
